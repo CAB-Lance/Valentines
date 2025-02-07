5cfdeba6cccfb022880adceed103493804ce8a22
CAB-Lance <lancechristianpallescocab@gmail.com>
2025-02-07 14:39:39 +0800

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        popUp: 'popUp 0.5s ease-out forwards',
+      },
+      keyframes: {
+        popUp: {
+          '0%': {
+            transform: 'scale(0.5)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'scale(1)',
+            opacity: '1',
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
